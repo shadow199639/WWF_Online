072e999525da4887899e3e9d20e8f7d6747b9ae3
shadow199639 <claud_96@mail.ru>
2020-04-06 21:19:27 +0300

    New AnimalDetailsActivity

---
+++ app/src/main/java/com/example/wwfonline/ui/review/AnimalDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.wwfonline.ui.review;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wwfonline.R;
+
+public class AnimalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_details);
+    }
+}
--- app/src/main/java/com/example/wwfonline/ui/review/AnimalListActivity.java
+++ app/src/main/java/com/example/wwfonline/ui/review/AnimalListActivity.java
@@ -2,7 +2,10 @@ package com.example.wwfonline.ui.review;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -42,7 +45,23 @@ public class AnimalListActivity extends AppCompatActivity {
                                 Toast.makeText(getApplicationContext(), "Load Successful", Toast.LENGTH_SHORT).show();
                                 animalDetailsModels = (List<AnimalDetailsModel>) response.body();
                                 listViewAnimalDetails.setAdapter(new AnimalDetailsAdapter(getApplicationContext(), animalDetailsModels));
-
+                                listViewAnimalDetails.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                    @Override
+                                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                        AnimalDetailsModel animalDetailsModel = animalDetailsModels.get(position);
+                                        Intent intent = new Intent(AnimalListActivity.this, AnimalDetailsActivity.class);
+                                        intent.putExtra("id", animalDetailsModel.getAnimalId());
+                                        intent.putExtra("animal_name", animalDetailsModel.getAnimalName());
+                                        intent.putExtra("desp", animalDetailsModel.getDescription());
+                                        intent.putExtra("habitat", animalDetailsModel.getHabitat());
+                                        intent.putExtra("population", animalDetailsModel.getPopulation());
+                                        intent.putExtra("place", animalDetailsModel.getPlace());
+                                        intent.putExtra("img", animalDetailsModel.getImg());
+                                        intent.putExtra("status", animalDetailsModel.getStatusName());
+                                        intent.putExtra("category", animalDetailsModel.getCatName());
+                                        startActivity(intent);
+                                    }
+                                });
                             } else {
                                 Toast.makeText(getApplicationContext(), "Server is not returned any data", Toast.LENGTH_SHORT).show();
                             }
